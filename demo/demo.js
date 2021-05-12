0e7428bfc562c23f0a0e332c4fd3bb24e198b7a3
Massimo Zambelli <mass.zambelli@gmail.com>
2021-05-12 18:29:13 +0200

    Demo site UI + JS

--- demo/demo.js
+++ demo/demo.js
@@ -5,14 +5,69 @@ import './demo.css';
 
 // buttons
 const initButton = document.querySelector('#init-hotjar-button');
+const identifyButton = document.querySelector('#identify-hotjar-button');
+const urlButton = document.querySelector('#url-hotjar-button');
+
+const setStatusMessage = (selector, type, message) => {
+  const statusBox = document.querySelector(selector);
+  statusBox.classList.remove('alert-danger', 'alert-success', 'hidden');
+  statusBox.classList.add(`alert-${type}`);
+  statusBox.textContent = message;
+};
+
+const enableHotjarFeatures = () => {
+  identifyButton.disabled = false;
+  urlButton.disabled = false;
+};
+
+const setHotjarInitialized = () => {
+  enableHotjarFeatures();
+  setStatusMessage('#init-hotjar-alert', 'success', 'Hotjar is initialized!');
+};
 
 // init button clicked
 initButton.addEventListener(
   'click',
   () => {
-    const siteID = 123;
+    const siteId = 2397592;
     const hotjarVersion = 6;
-    Hotjar.init(siteID, hotjarVersion);
+    Hotjar.init(siteId, hotjarVersion);
+
+    setHotjarInitialized();
+  },
+  false,
+);
+
+// identify button clicked
+identifyButton.addEventListener(
+  'click',
+  () => {
+    const userId = '_' + Math.random().toString(36).substr(2, 9);
+    const firstName = document.querySelector('#identify-hotjar-name').value || 'John';
+    const favoriteColor = document.querySelector('#identify-hotjar-color').value || 'blue';
+
+    Hotjar.identify(userId, {
+      first_name: firstName,
+      color: favoriteColor,
+    });
+
+    setStatusMessage(
+      '#identify-hotjar-alert',
+      'success',
+      `${firstName} identified with ${favoriteColor} as favorite color`,
+    );
+  },
+  false,
+);
+
+// url button clicked
+urlButton.addEventListener(
+  'click',
+  () => {
+    const newPage = '/new';
+    Hotjar.stateChange(newPage);
+
+    setStatusMessage('#url-hotjar-alert', 'success', 'The current page is /new');
   },
   false,
 );
